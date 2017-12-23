94368a5e9f61f80056d6b1d0f20fe6562edd5f1c
Bernard Tison <bernard.tison@gmail.com>
2017-12-23 14:57:43 +0100

    use CachingConnectionFactory for caching JMS connections and sessions

--- src/main/java/com/redhat/btison/enmasse/producer/jms/sb/ApplicationConfiguration.java
+++ src/main/java/com/redhat/btison/enmasse/producer/jms/sb/ApplicationConfiguration.java
@@ -1,7 +1,13 @@
 package com.redhat.btison.enmasse.producer.jms.sb;
 
+import javax.jms.JMSException;
+
+import org.apache.qpid.jms.JmsConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.connection.CachingConnectionFactory;
+import org.springframework.jms.core.JmsTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
@@ -10,11 +16,24 @@ import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 @Configuration
 public class ApplicationConfiguration {
 
+    @Autowired
+    private JmsConnectionFactory jcf;
+
+
     @Bean
     public JacksonJsonProvider jsonProvider(ObjectMapper objectMapper) {
         JacksonJaxbJsonProvider provider = new JacksonJaxbJsonProvider();
         provider.setMapper(objectMapper);
         return provider;
+    }
+
+    @Bean
+    JmsTemplate jmsTemplate() throws JMSException {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(jcf);
+        connectionFactory.setSessionCacheSize(50);
+
+        JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory);
+        return jmsTemplate;
     }
 
 }
